dee1dd413d14a3e2b7f776a9cf16f244a9a759c7
Jason Basuil <basuilj@gmail.com>
2018-11-28 06:05:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  list = []
+  attendees.each do |i| 
+    list.push(badge_maker(i))
+  end
+  return list
+end 
+
+def assign_rooms(speakers)
+  assignment = []
+  speakers.each_with_index do |i, index |
+    assignment.push("Hello, #{i}! You'll be assigned to room #{index}")
+  end
+  return assignment
+end 
+
+  
+  
